4b386b325a0d2b68f2b8f634a2966b06cd334308
pacrae359 <paulmmacrae@gmail.com>
2024-02-03 13:50:18 +0000

    Generic Detail Vote and Results complete

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
+from django.db.models import F
+
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 from django.urls import reverse
@@ -21,8 +23,8 @@ def detail(request, question_id):
 	return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
-	response = "You're looking at the results of question %s."
-	return HttpResponse(response % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, "polls/results.html", {"question":question})
 
 def vote(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
@@ -38,7 +40,8 @@ def vote(request, question_id):
 			},
 		)
 	else:
-		selected_choice.votes += 1
+		# F() is used to avoid a race condition so multiple users can update values at
+		# the same time and the correct value will be stored.
+		selected_choice.votes = F("votes") + 1
 		selected_choice.save()
 		return HttpResponseRedirect(reverse("polls:results",args=(question.id,)))
-
